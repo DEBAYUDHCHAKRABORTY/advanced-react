51aabe64e7008b8e1341b7fa685c452c60de000a
Pankaj Pramanik <pankajpramanik002@gmail.com>
2025-09-23 20:34:55 +0530

    Sonu (#3)
    * added files
    
    * added compo1
    
    * added app.jsx

--- sonu/src/App.jsx
+++ sonu/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Greet from './compo/com1'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Greet/>
     </>
   )
 }
