b545d4db1601abf0c565691b9dce694f10aea0dd
DC <debayudh.maldaitbss@gmail.com>
2025-09-23 20:48:32 +0530

    Second Comp done

--- react/advanced/src/App.jsx
+++ react/advanced/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Comp from './component'
+import Comp2 from './Comp2'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Comp/>
+      <Comp2/>
     </>
   )
 }
---
+++ react/advanced/src/comp2.jsx
@@ -0,0 +1,9 @@
+function Comp2(){
+    return(
+        <div>
+            <h1>Second Comp</h1>
+        </div>
+    )
+}
+
+export default Comp2;
