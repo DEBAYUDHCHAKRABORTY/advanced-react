e7e6c504f95f5680886baa7cdc6347217b9ec2be
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-09-23 21:04:36 +0530

    added app and com3

--- sonu/src/App.jsx
+++ sonu/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Add from './compo/comp2'
+import Multi from './compo/com3'
 import './App.css'
 
 import Greet from './compo/com1'
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
      <Greet/>
-
+     <Multi/>
      <Add/>
     </>
   )
