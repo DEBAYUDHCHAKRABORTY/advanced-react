b2aa03850c7fbb7da8971b9dda1e4a7cbb3491b2
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-09-23 21:27:21 +0530

    updated cpmo4

--- sonu/src/App.jsx
+++ sonu/src/App.jsx
@@ -7,6 +7,7 @@ import Multi from './compo/com3'
 import './App.css'
 
 import Greet from './compo/com1'
+import Count from './compo/comp4'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,6 +16,7 @@ function App() {
      <Greet/>
      <Multi/>
      <Add/>
+     <Count/>
     </>
   )
 }
