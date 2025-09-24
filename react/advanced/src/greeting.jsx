303f3abcaf7f80d0d38162fe699851ade79251c8
DC74 <debayudh.maldaitbss@gmail.com>
2025-09-24 20:23:53 +0530

    React/czar (#10)
    * third day
    
    * Second Comp done
    
    * Count App Done
    
    * increament & decreamnet done
    
    * morning done

---
+++ react/advanced/src/greeting.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function Greet(){
+
+    const[name,setName] = useState('')
+    const[greet,setGreet] = useState('Hello')
+
+    function morning(){
+        setGreet("Good Morning")
+    }
+    
+    function handleGreet(event){
+        setName(event.target.value)
+    }
+
+
+
+    return(
+        <div>
+            <input type="text" placeholder="Enter your name !" onInput={handleGreet}/>
+            <button onClick={morning}>morning</button>
+            <h3>{greet} {name}</h3>
+        </div>
+    )
+}
+
+export default Greet;
